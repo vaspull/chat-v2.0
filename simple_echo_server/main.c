a88114d55b0314cd2c7bf775454e80793665f96b
vaspull <vaspull@mail.ru>
2020-01-20 15:21:07 +0300

    cosmetic fixes

--- simple_echo_server/main.c
+++ simple_echo_server/main.c
@@ -3,9 +3,9 @@
 
 int main()
 {
-    unsigned int connection_count = 0;
-    SOCKET* Connections;
+    u_int connection_count = 0;
     u_long iMode = 1;
+    SOCKET* Connections;
     SOCKET Connect;
     Connections = (SOCKET*)calloc(64,sizeof(SOCKET));
     WSADATA wsaData;
@@ -18,7 +18,7 @@ int main()
     hints.ai_protocol = IPPROTO_TCP;
     hints.ai_flags = AI_PASSIVE;
     getaddrinfo(0,"7770", &hints, &addr);
-    unsigned int listen_socket = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+    u_int listen_socket = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
     bind(listen_socket,addr->ai_addr,(int)addr->ai_addrlen);
     listen(listen_socket, SOMAXCONN);
     ioctlsocket(listen_socket, (long)FIONBIO, &iMode);
@@ -27,14 +27,14 @@ int main()
         if((Connect = accept(listen_socket,NULL,NULL)) != INVALID_SOCKET) {
             Connections[connection_count++] = Connect;
         }
-        for(unsigned int i = 0; i <= connection_count; ++i) {
+        for(u_int i = 0; i <= connection_count; ++i) {
             char buffer[500];
-            for(unsigned int a = 0; a <= sizeof(buffer); ++a) {
+            for(u_int a = 0; a <= sizeof(buffer); ++a) {
                 buffer[a] = '\0';
             }
             if (Connections[i] != 4294967295 && Connections[i] > 0) {
                 if ((bytes_read = recv(Connections[i], buffer, sizeof(buffer),0)) != SOCKET_ERROR ) {
-                    for (unsigned int b = 0;b <= connection_count;++b) {
+                    for (u_int b = 0;b <= connection_count;++b) {
                         if (Connections[b]) {
                             if (Connections[b]) {
                                 if ((bytes_write = send(Connections[b],buffer,(int)strlen(buffer),0)) == -1) {

45015aed29b861b39d3aa60d1933b004c10d7357
vaspull <vaspull@mail.ru>
2020-01-21 13:02:41 +0300

    fixes

--- simple_echo_server/main.c
+++ simple_echo_server/main.c
@@ -3,11 +3,11 @@
 
 int main()
 {
-    u_int connection_count = 0;
+    u_int connection_count = 0, max_conn = 32;
     u_long iMode = 1;
     SOCKET* Connections;
     SOCKET Connect;
-    Connections = (SOCKET*)calloc(64,sizeof(SOCKET));
+    Connections = (SOCKET*)calloc(max_conn,sizeof(SOCKET));
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
     struct addrinfo* addr = NULL;
@@ -24,21 +24,25 @@ int main()
     ioctlsocket(listen_socket, (long)FIONBIO, &iMode);
     for (;;Sleep(1)) {
         int bytes_read, bytes_write;
-        if((Connect = accept(listen_socket,NULL,NULL)) != INVALID_SOCKET) {
-            Connections[connection_count++] = Connect;
+        if((Connect = accept(listen_socket,NULL,NULL)) != INVALID_SOCKET && connection_count < max_conn) {
+            u_int x;
+            for (x = 0; Connections[x]; ++x);
+            Connections[x] = Connect;
+            ++connection_count;
         }
-        for(u_int i = 0; i <= connection_count; ++i) {
+        for(u_int i = 0; i <= max_conn; ++i) {
             if (Connections[i]) {
                 char buffer[500];
                 for(u_int a = 0; a <= sizeof(buffer); ++a) {
                     buffer[a] = '\0';
                 }
                 if ((bytes_read = recv(Connections[i], buffer, sizeof(buffer),0)) != SOCKET_ERROR ) {
-                    for (u_int b = 0;b <= connection_count;++b) {
+                    for (u_int b = 0;b <= max_conn;++b) {
                         if (Connections[b]) {
                             if ((bytes_write = send(Connections[b],buffer,(int)strlen(buffer),0)) == -1) {
                                 closesocket(Connections[b]);
-                                Connections[b] = '\0';
+                                Connections[b] = 0;
+                                --connection_count;
                             }
                         }
                     }
